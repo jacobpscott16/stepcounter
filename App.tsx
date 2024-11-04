90dcac95f4cded7eabdcd89493074838c076d1df
Jacob Scott <jacob.scott.everstox@jacobs-air.speedport.ip>
2024-11-04 15:27:16 +0100

    Render Text Stats

--- App.tsx
+++ App.tsx
@@ -1,7 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-const Value = ({label, value}) => (
+type ValueProps = {
+  label: string;
+  value: string;
+}
+
+
+const Value = ({label, value}: ValueProps) => (
   <View style={styles.valueContainer}>
     <Text style={styles.label}>{label}</Text>
     <Text style={styles.value}>{value}</Text>
