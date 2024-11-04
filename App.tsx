f239cd7aaea9a2c324bbfa95fcc8dcc62e30073b
Jacob Scott <jacob.scott.everstox@jacobs-air.speedport.ip>
2024-11-04 15:21:24 +0100

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+const Value = ({label, value}) => (
+  <View style={styles.valueContainer}>
+    <Text style={styles.label}>{label}</Text>
+    <Text style={styles.value}>{value}</Text>
+  </View>
+
+)
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello!  I am Jake!</Text>
+
+
+      <View style={{flexDirection: 'row'}}>
+
+        <Value label="Steps" value="1219" />
+        <Value label="Distance" value="0.75 km" />
+      </View>
+        <Value label="Flights Climbed" value="3 Floors" />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +30,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'black',
     justifyContent: 'center',
+    padding: 12, 
   },
+  valueContainer: {
+    marginRight: 50,
+    marginVertical: 10,
+  },
+  label: {
+    color: 'white',
+    fontSize: 20,
+  },
+  value: {
+    fontSize: 35,
+    color: '#AFB3BE',
+    fontWeight: '500',
+  },
+  
 });
