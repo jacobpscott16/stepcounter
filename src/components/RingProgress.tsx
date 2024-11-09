ff366f80f1530a006d5790a6726efd698bed6f5f
Jacob Scott <jacob.scott.everstox@jacobs-air.speedport.ip>
2024-11-09 12:10:49 +0100

    Ring progress ui

--- src/components/RingProgress.tsx
+++ src/components/RingProgress.tsx
@@ -1,6 +1,11 @@
 import { View, Text } from 'react-native';
-import SVG, { Circle } from 'react-native-svg';
-import Animated, { useAnimatedProps } from 'react-native-reanimated';
+import SVG, { Circle, CircleProps } from 'react-native-svg';
+import Animated, {
+    useAnimatedProps,
+    useSharedValue,
+    withTiming,
+} from 'react-native-reanimated';
+import { useEffect } from 'react';
 
 const AnimatedCircle = Animated.createAnimatedComponent(Circle);
 
@@ -21,32 +26,38 @@ const RingProgress = ({
     const innerRadius = radius - strokeWidth / 2;
     const circumference = 2 * Math.PI * innerRadius;
 
+    const fill = useSharedValue(0);
+
+    useEffect(() => {
+        fill.value = withTiming(progress, { duration: 1500 });
+    }, [progress]);
+
     const animatedProps = useAnimatedProps(() => ({
-        strokeDasharray: [circumference * progress, circumference]
+        strokeDasharray: [circumference * fill.value, circumference]
     }));
+
+    const circleDefaultProps: CircleProps = {
+        r: innerRadius,
+        cx: radius,
+        cy: radius,
+        originX: radius,
+        originY: radius,
+        strokeWidth: strokeWidth,
+        stroke: color,
+        strokeLinecap: 'round',
+    };
 
     return (
         <View style={{ width: radius * 2, height: radius * 2, alignSelf: 'center' }}>
             <SVG>
                 {/* Background */}
                 <Circle
-                    cx={radius}
-                    cy={radius}
-                    r={innerRadius}
-                    strokeWidth={strokeWidth}
-                    stroke={color}
+                    {...circleDefaultProps}
                     opacity={0.2} />
                 {/* Foreground */}
                 <AnimatedCircle
                     animatedProps={animatedProps}
-                    r={innerRadius}
-                    cx={radius}
-                    cy={radius}
-                    originX={radius}
-                    originY={radius}
-                    strokeWidth={strokeWidth}
-                    stroke={color}
-                    strokeLinecap='round'
+                    {...circleDefaultProps}
                     rotation="-90"
 
                 />
