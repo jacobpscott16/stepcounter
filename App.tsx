dc544bcb050883c3705d48c8e6fc58cd40d8f1ab
Jacob Scott <jacob.scott.everstox@jacobs-air.speedport.ip>
2024-11-10 12:24:56 +0100

    Values customs

--- App.tsx
+++ App.tsx
@@ -2,87 +2,27 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Value from './src/components/Value'
 import RingProgress from './src/components/RingProgress';
-import AppleHealthKit, { HealthInputOptions, HealthKitPermissions} from 'react-native-health';
-import { useEffect, useState } from 'react';
-
-const permissions: HealthKitPermissions = {
-  permissions: {
-    read: [AppleHealthKit.Constants.Permissions.Steps, AppleHealthKit.Constants.Permissions.FlightsClimbed, AppleHealthKit.Constants.Permissions.DistanceWalkingRunning,],
-    write: [],
-  }
-};
+import useHealthData from './src/Hooks/useHealthData';
 
 const STEPS_GOAL = 10_000;
 
 export default function App() {
-  const [hasPermissions, setHasPermission] = useState(false);
-  const [steps, setSteps] = useState(0);
-  const [flights, setFlights] = useState(0);
-  const [distance, setDistance] = useState(0);
+  const { steps, flights, distance } = useHealthData(new Date());
 
-  useEffect(() => {
-    AppleHealthKit.initHealthKit(permissions, (err) => {
-      if (err) {
-        console.log('Error getting persmissions');
-        return;
-      }
-      setHasPermission(true);
-    });
-  }, []);
+  return (
+    <View style={styles.container}>
+      <RingProgress radius={125} strokeWidth={50} progress={steps / STEPS_GOAL} />
 
-  useEffect(() => {
-    if (!hasPermissions) {
-      return;
-    }
+      <View style={styles.values}>
+        <Value label="Steps" value={steps.toString()} />
+        <Value label="Distance" value={`${(distance / 1000).toFixed(2)} km`} />
+        <Value label="Flights Climbed" value={flights.toString()} />
 
-    const options: HealthInputOptions = {
-      date: new Date().toISOString(),
-      includeManuallyAdded: false,
-    }
+      </View>
 
-    AppleHealthKit.getStepCount(options, (err, results) => {
-      if (err) {
-        console.log("Error getting the steps");
-        return;
-      }
-      console.log(results.value);
-      setSteps(results.value);
-    });
-
-  AppleHealthKit.getFlightsClimbed(options, (err, results) => {
-    if (err) {
-      console.log("Error getting the flights");
-      return;
-    }
-    console.log(results.value);
-    setFlights(results.value);
-  });
-
-  AppleHealthKit.getDistanceWalkingRunning(options, (err, results) => {
-    if (err) {
-      console.log("Error getting the distance");
-      return;
-    }
-    console.log(results.value);
-    setDistance(results.value);
-  });
-}, [hasPermissions]);
-
-return (
-  <View style={styles.container}>
-    <RingProgress radius={125} strokeWidth={50} progress={steps / STEPS_GOAL} />
-
-
-    <View style={styles.values}>
-      <Value label="Steps" value={steps.toString()} />
-      <Value label="Distance" value={`${(distance / 1000).toFixed(2)} km`} />
-      <Value label="Flights Climbed" value={flights.toString()} />
-
+      <StatusBar style="auto" />
     </View>
-
-    <StatusBar style="auto" />
-  </View>
-);
+  );
 }
 
 const styles = StyleSheet.create({
---
+++ src/Hooks/useHealthData.tsx
@@ -0,0 +1,73 @@
+import AppleHealthKit, { HealthInputOptions, HealthKitPermissions } from 'react-native-health';
+import { useEffect, useState } from 'react';
+
+const permissions: HealthKitPermissions = {
+  permissions: {
+    read: [AppleHealthKit.Constants.Permissions.Steps, AppleHealthKit.Constants.Permissions.FlightsClimbed, AppleHealthKit.Constants.Permissions.DistanceWalkingRunning,],
+    write: [],
+  }
+};
+
+const useHealthData = (date: Date) => {
+
+    const [hasPermissions, setHasPermission] = useState(false);
+    const [steps, setSteps] = useState(0);
+    const [flights, setFlights] = useState(0);
+    const [distance, setDistance] = useState(0);
+  
+    useEffect(() => {
+      AppleHealthKit.initHealthKit(permissions, (err) => {
+        if (err) {
+          console.log('Error getting persmissions');
+          return;
+        }
+        setHasPermission(true);
+      });
+    }, []);
+  
+    useEffect(() => {
+      if (!hasPermissions) {
+        return;
+      }
+  
+      const options: HealthInputOptions = {
+        date: date.toISOString(),
+        includeManuallyAdded: false,
+      }
+  
+      AppleHealthKit.getStepCount(options, (err, results) => {
+        if (err) {
+          console.log("Error getting the steps");
+          return;
+        }
+        console.log(results.value);
+        setSteps(results.value);
+      });
+  
+      AppleHealthKit.getFlightsClimbed(options, (err, results) => {
+        if (err) {
+          console.log("Error getting the flights");
+          return;
+        }
+        console.log(results.value);
+        setFlights(results.value);
+      });
+  
+      AppleHealthKit.getDistanceWalkingRunning(options, (err, results) => {
+        if (err) {
+          console.log("Error getting the distance");
+          return;
+        }
+        console.log(results.value);
+        setDistance(results.value);
+      });
+    }, [hasPermissions]);
+
+    return {
+        steps,
+        flights,
+        distance,
+    };
+};
+
+export default useHealthData;
